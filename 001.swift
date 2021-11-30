b62746b2011bfe3840207c56b4ae92ea0815e3c2
samekard-dev <88572321+samekard-dev@users.noreply.github.com>
2021-11-30 19:47:07 +0900

    コメント追加

--- 001.swift
+++ 001.swift
@@ -13,6 +13,7 @@ let n = nl[0]  //切れ目の数
 let l = nl[1]  //羊羹の長さ
 let k = readInt()  //採用する切れ目の数
 let a = readIntArray()  //各切れ目の左からの長さ
+
 func solve(m: Int) -> Bool {
 	var cnt = 0, pre = 0
 	for i in 0..<n {  //c++の方では1...nとなっているが俯瞰で見るとやっていることは同じ
@@ -30,7 +31,7 @@ func solve(m: Int) -> Bool {
 //この問題だけでなく他の2分探索にも応用できるような初期値の選び方かと
 var left = -1  
 var right = l + 1
-while right - left > 1 {
+while right - left > 1 {  //最後は隣り合った2つになって抜ける
 	let mid = left + (right - left) / 2
 	if solve(m: mid) == false {
 		right = mid
