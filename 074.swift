460e9cd3a27c25b122222921e75691fefa2c89f4
samekard-dev <88572321+samekard-dev@users.noreply.github.com>
2022-05-24 22:52:31 +0900

    コメント変更

--- 074.swift
+++ 074.swift
@@ -17,7 +17,7 @@ for i in 0..<n {
 			counter += 1
 			//iは最大59なので、pow計算した結果のDoubleの値が、
 			//全てのIntを正確に現せる範囲を超えるが、
-			//2の倍数なので正確さを維持する
+			//2のべき乗なので正確さを維持する
 			counter += Int(pow(2.0, Double(i))) - 1
 		case "c":
 			counter += 2
