7f8a4208db131b69a77a8fc8e890b6ccbc38d0ea
samekard-dev <88572321+samekard-dev@users.noreply.github.com>
2023-11-16 00:16:43 +0900

    Update 002.swift
    iの2重定義を解消

--- 002.swift
+++ 002.swift
@@ -1,40 +1,39 @@
 func readInt() -> Int {
-	Int(readLine()!)!
+    Int(readLine()!)!
 }
 
 func hantei(s: [Character]) -> Bool {
-	var dep = 0; //depth?
-	for c in s {
-		if c == "(" {
-			dep += 1
-		} else if c == ")" {
-			dep -= 1
-		}
-		if dep < 0 {
-			return false
-		}
-	}
-	if dep == 0 {
-		return true
-	} else {
-		return false
-	}
+    var dep = 0; //depth?
+    for c in s {
+        if c == "(" {
+            dep += 1
+        } else if c == ")" {
+            dep -= 1
+        }
+        if dep < 0 {
+            return false
+        }
+    }
+    if dep == 0 {
+        return true
+    } else {
+        return false
+    }
 }
 
 let n = readInt() //1 ≦ n ≦ 20
 for i in 0..<(1 << n) { //n = 1なら0..<2
-	var candidate: [Character] = []
-	for j in (0..<n).reversed() {
-		// メモ : (i & (1 << j)) = 0 というのは、i の j ビット目（2^j の位）が 0 であるための条件。
-		// 　　　頻出なので知っておくようにしましょう。		
-		if i & (1 << j) == 0 {
-			candidate.append("(")
-		} else {
-			candidate.append(")")
-		}
-	}
-	let i = hantei(s: candidate)
-	if i {
-		print(candidate.map({ String($0) }).joined())
-	}
+    var candidate: [Character] = []
+    for j in (0..<n).reversed() {
+        // メモ : (i & (1 << j)) = 0 というのは、i の j ビット目（2^j の位）が 0 であるための条件。
+        // 　　　頻出なので知っておくようにしましょう。        
+        if i & (1 << j) == 0 {
+            candidate.append("(")
+        } else {
+            candidate.append(")")
+        }
+    }
+    if hantei(s: candidate) {
+        print(candidate.map({ String($0) }).joined())
+    }
 }
