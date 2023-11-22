c192b8a73351880f7f93a5917141e830495cec4d
samekard-dev <88572321+samekard-dev@users.noreply.github.com>
2023-11-22 18:21:33 +0900

    Update 072.swift スタート地点で山かどうか見るように

--- 072.swift
+++ 072.swift
@@ -1,14 +1,14 @@
-func readIntArray() -> [Int] {
-	readLine()!.split(separator: " ").map { Int(String($0))! }
+func readInts() -> [Int] {
+    readLine()!.split(separator: " ").map { Int(String($0))! }
 }
 
-let hw = readIntArray()
+let hw = readInts()
 let h = hw[0]
 let w = hw[1]
 
 var geo: [[Character]] = []
 for _ in 1...h {
-	geo.append(Array(readLine()!))
+    geo.append(Array(readLine()!))
 }
 
 var passed = [[Bool]](repeating: [Bool](repeating: false, count: w), count: h)
@@ -19,43 +19,42 @@ var originY = 0
 var counter = -1
 
 for y in 0..<h {
-	for x in 0..<w {
-		originX = x
-		originY = y
-		for (dX, dY) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-			let nextX = originX + dX
-			let nextY = originY + dY
-			if nextX >= 0 && nextX <= w - 1
-				&& nextY >= 0 && nextY <= h - 1
-				&& geo[nextY][nextX] == "." {
-				explore(x: nextX, y: nextY, times: 1)
-			}
-		}
-	}
+    for x in 0..<w {
+        if geo[y][x] == "#" {
+            continue
+        }
+        originX = x
+        originY = y
+        seeFrom(x: x, y: y, times: 0)
+    }
 }
 
-func explore(x: Int, y: Int, times: Int) {
-	if x == originX && y == originY {
-		if times >= 3 {
-			if times > counter {
-				counter = times
-			}
-		}
-		return
-	}
-	
-	passed[y][x] = true
-	for (dX, dY) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-		let nextX = x + dX
-		let nextY = y + dY
-		if nextX >= 0 && nextX <= w - 1
-			&& nextY >= 0 && nextY <= h - 1
-			&& geo[nextY][nextX] == "."
-			&& passed[nextY][nextX] == false {
-			explore(x: nextX, y: nextY, times: times + 1)
-		}
-	}
-	passed[y][x] = false
+func seeFrom(x: Int, y: Int, times: Int) {
+    for (dX, dY) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+        let nextX = x + dX
+        let nextY = y + dY
+        if nextX >= 0 && nextX <= w - 1
+            && nextY >= 0 && nextY <= h - 1
+            && geo[nextY][nextX] == "."
+            && passed[nextY][nextX] == false {
+            walkTo(x: nextX, y: nextY, times: times + 1)
+        }
+    }
+}
+
+func walkTo(x: Int, y: Int, times: Int) {
+    if x == originX && y == originY {
+        if times >= 3 {
+            if times > counter {
+                counter = times
+            }
+        }
+        return
+    }
+    
+    passed[y][x] = true
+    seeFrom(x: x, y: y, times: times)
+    passed[y][x] = false
 }
 
 print(counter)
